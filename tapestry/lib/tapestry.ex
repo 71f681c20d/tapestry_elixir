5c85c57c00857a3524131a7a124c06af1b7e15eb
Porthos <leedeffebach@gmail.com>
2019-10-22 12:22:10 -0400

    working on init nodes

--- tapestry/lib/tapestry.ex
+++ tapestry/lib/tapestry.ex
@@ -9,31 +9,22 @@ defmodule Tapestry do
         num_nodes = String.to_integer(num_nodes)
         #num_requests = String.to_integer(num_requests)
         Tapestry.DynamicSupervisor.start_link(args)     # TODO change this
-        nodes = Tapestry.DynamicSupervisor.start_children(num_nodes, [])
-        nodes
-        # Enum.map(nodes, fn x -> init_node(x))         # topology is self-organizing. nodes only need to be initialized
+        #Enum.map(tl, fn x -> init_node(x, hd) end)         # topology is self-organizing. nodes only need to be initialized
 
 
-        #[hd, snd, thrd, frth, fifth | _tl] = Tapestry.DynamicSupervisor.start_children(num_nodes, [])
-        #Tapestry.Server.join_from(frth, snd)
-        #Tapestry.Server.join_from(thrd, snd)
-        #Tapestry.Server.join_from(fifth, thrd)
-        #Tapestry.Server.join_from(hd, snd)
-        #Tapestry.Server.get_neighbors(hd) #Should have uid 2, 3, 4, 5 as neighbors
+        [hd, snd, thrd, frth, fifth | _tl] = Tapestry.DynamicSupervisor.start_children(num_nodes, [])
+        Tapestry.Server.join_from(frth, snd)
+        Tapestry.Server.join_from(thrd, snd)
+        Tapestry.Server.join_from(fifth, thrd)
+        Tapestry.Server.join_from(hd, snd)
+        Tapestry.Server.get_neighbors(hd) #Should have uid 2, 3, 4, 5 as neighbors
       _ ->
         IO.puts 'Invalid arguments please put args: numNodes numRequests'
     end
   end
 
-  defp init_node(node) do
-    #TODO: build out the DHT of new nodes coming on the network
-    # 1.
-    :ok
-  end
-
-  defp join_nodes([]), do: :ok
-  defp join_nodes(list) do
-    [h|t]=list
+  defp init_node(node, network_node) do
+    Tapestry.Server.join_from(node, network_node)
   end
 
 end
