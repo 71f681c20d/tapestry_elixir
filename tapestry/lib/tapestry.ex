caa9aa587dcac6cf38c9537484365d41c52c34ad
Porthos <leedeffebach@gmail.com>
2019-10-22 12:39:13 -0400

    Tapestry generates

--- tapestry/lib/tapestry.ex
+++ tapestry/lib/tapestry.ex
@@ -1,7 +1,6 @@
 defmodule Tapestry do
 
   def start(_type, _args) do
-    IO.puts("hello")
     #args = System.argv()
     args = ["10", "10"]
     case args do
@@ -9,15 +8,16 @@ defmodule Tapestry do
         num_nodes = String.to_integer(num_nodes)
         #num_requests = String.to_integer(num_requests)
         Tapestry.DynamicSupervisor.start_link(args)
-        [hd, snd, thrd, frth, fifth | _tl] = Tapestry.DynamicSupervisor.start_children(num_nodes, [])
-        Tapestry.Server.join_from(frth, snd)
-        Tapestry.Server.join_from(thrd, snd)
-        Tapestry.Server.join_from(fifth, thrd)
-        Tapestry.Server.join_from(hd, snd)
+        [hd | tl] = Tapestry.DynamicSupervisor.start_children(num_nodes, [])
+        Enum.map(tl, fn x -> init_tapestry(x, hd) end)
         Tapestry.Server.get_neighbors(hd) #Should have uid 2, 3, 4, 5 as neighbors
       _ ->
         IO.puts 'Invalid arguments please put args: numNodes numRequests'
     end
+  end
+
+  def init_tapestry(node, node_in_network) do
+    Tapestry.Server.join_from(node, node_in_network)
   end
 
 end
