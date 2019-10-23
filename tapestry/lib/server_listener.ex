7af744edebe706c23afa34ff96391da3f3998d93
alexwb1 <abarnard1@ufl.edu>
2019-10-23 17:51:44 -0400

    pushing changes

--- tapestry/lib/server_listener.ex
+++ tapestry/lib/server_listener.ex
@@ -9,16 +9,16 @@ end
     {:ok, state}
   end
 
-  def handle_cast({:found, jumps, to}, state) do
+  def handle_cast({:found, jumps, to}, state) do  # Gets called when message is delivered to corret peer
     num_received = elem(Map.fetch(state, :num_received), 1) + 1
     num_expected = elem(Map.fetch(state, :num_expected), 1)
     cond do
       num_received == num_expected ->
-        IO.inspect(Enum.join(["done", to], " "))
+        IO.inspect(Enum.join(["done1", to], " "))
         IO.puts 'terminating'
         Tapestry.DynamicSupervisor.terminate_child(self())
       true ->
-        IO.inspect(Enum.join(["done", to, Integer.to_string(jumps)], " "))
+        IO.inspect(Enum.join(["done2", to, Integer.to_string(jumps)], " "))
         state = Map.put(state, :num_received, num_received)
         {:noreply, state}
     end
